73ba564ad96cd0357311e18da5bf0e5cab1ea232
Mehedi Hasan Siddique <hasanmehidi2016@gmail.com>
2025-12-01 19:07:50 +0600

    Delete Hello_java.java

--- Hello_java.java
+++
@@ -1,9 +0,0 @@
-package hello_java;
-         // This is the class named Hello_java (file name should be Hello_java.java)
-public class Hello_java {
-         // This is the main method — program starts running from here
-    public static void main(String[] args) {
-        System.out.println("Hello Java");      // This prints text to the screen
-        // TODO code application logic here
-    }  
-}
